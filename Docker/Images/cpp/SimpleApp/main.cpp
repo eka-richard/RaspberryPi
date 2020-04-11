a486484330598ead92c99188709d1bc0ce6926e5
eka-richard <eka.richard@hotmail.com>
2020-04-11 23:24:45 +0800

    Add Multithreading

--- Docker/Images/cpp/SimpleApp/main.cpp
+++ Docker/Images/cpp/SimpleApp/main.cpp
@@ -1,8 +1,37 @@
 #include "main.h"
 
+void Task_500msCyclic()
+{
+    while (1)
+    {
+        printf("Running 500 ms cyclic task...\n");
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+}
+
+void Task_1sCyclic()
+{
+    while (1)
+    {
+        printf("Running 1 s cyclic task...\n");
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
 int main()
 {
-	printf("The number is: %d", NUMBER);
+    std::thread first(Task_500msCyclic);
+    std::thread second(Task_1sCyclic);
 
-	return 0;
+    printf("Threads has been created, now spawning...\n");
+
+    first.join();
+    second.join();
+
+    printf("Finish running");
+
+    return 0;
 }
