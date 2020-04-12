58a70463a34ee380cfbe6c00a4ecac9e9e9f525a
eka-richard <eka.richard@hotmail.com>
2020-04-12 09:13:36 +0800

    Applying detached thread

--- Docker/Images/cpp/SimpleApp/main.cpp
+++ Docker/Images/cpp/SimpleApp/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 #include "main.h"
 
 void Task_500msCyclic()
@@ -8,7 +11,6 @@ void Task_500msCyclic()
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
-
 }
 
 void Task_1sCyclic()
@@ -23,15 +25,34 @@ void Task_1sCyclic()
 
 int main()
 {
+    unsigned int totalDuration = 0;
+    std::chrono::system_clock::time_point storedTime = std::chrono::system_clock::now();
+
     std::thread first(Task_500msCyclic);
     std::thread second(Task_1sCyclic);
 
     printf("Threads has been created, now spawning...\n");
 
-    first.join();
-    second.join();
+    first.detach();
+    second.detach();
 
-    printf("Finish running");
+    printf("Finish spawning all task...\n");
+
+    while (totalDuration < 10)
+    {
+        std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
+        std::chrono::system_clock::duration elapsedTime = currentTime - storedTime;
+
+        if (elapsedTime.count() > 10000000)
+        {
+            totalDuration++;
+            std::cout << "We have been running for: " << totalDuration << " seconds" << std::endl;
+
+            storedTime = currentTime;
+        }
+    }
+
+    std::cout << "Timer expired terminating all task..." << std::endl;
 
     return 0;
 }
--- Docker/Images/cpp/SimpleApp/main.h
+++ Docker/Images/cpp/SimpleApp/main.h
@@ -1,6 +1,3 @@
 #pragma once
-#include <iostream>
-#include <thread>
-#include <chrono>
 
 #define NUMBER 7
